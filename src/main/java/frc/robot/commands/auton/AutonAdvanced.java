583b73bc17f56cc9c2b9c2b185c0332d0c433cfd
John Doe <23olivera@cistercianmail.net>
2022-03-15 17:45:32 -0500

    auton diff

--- src/main/java/frc/robot/commands/auton/AutonAdvanced.java
+++ src/main/java/frc/robot/commands/auton/AutonAdvanced.java
@@ -1,34 +1,39 @@
 package frc.robot.commands.auton;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.chassis.ChassisDriveAuton;
 import frc.robot.commands.chassis.ChassisDriveAutonFC;
 import frc.robot.commands.chassis.ResetGyro;
 import frc.robot.commands.chassis.ToggleAim;
-import frc.robot.commands.shooter.RunShooterVelocity;
+import frc.robot.commands.intake.RunIntakeTimed;
 import frc.robot.commands.shooter.RunShooterVelocityTimed;
 import frc.robot.subsystems.Chassis;
 import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
-public class AutonBasic extends SequentialCommandGroup {
+public class AutonAdvanced extends SequentialCommandGroup {
 
     Chassis mChassis;
     Shooter mShooter;
     Indexer mIndexer;
+    Intake mIntake;
 
-    public AutonBasic(Chassis chassis, Shooter shooter, Indexer indexer) {
+    public AutonAdvanced(Chassis chassis, Shooter shooter, Indexer indexer, Intake intake) {
 
 
         mChassis = chassis;
         mShooter = shooter;
         mIndexer = indexer;
+        mIntake = intake;
 
         addRequirements(mChassis, mShooter, mIndexer);
 
         addCommands(new ResetGyro(mChassis),
                 new AutonTransition(mChassis, 0.0, 0.0, 0.0, -0.4, 0.0, 0.0, 0.5),
-                new ChassisDriveAuton(mChassis, -0.4, 0.0, 0.0, 0.5),
+                new ParallelCommandGroup(new ChassisDriveAuton(mChassis, -0.4, 0.0, 0.0, 0.5),
+                        new RunIntakeTimed(mIntake, 0.5)),
                 new AutonTransition(mChassis, -0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5),
                 new ToggleAim(mChassis),
                 new ChassisDriveAutonFC(mChassis, 0.0, 0.0, 0.0, 0.5),
@@ -36,5 +41,6 @@ public class AutonBasic extends SequentialCommandGroup {
         );
 
     }
+
 
 }

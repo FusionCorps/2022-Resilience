f8677e1ea72f241402cdabfb2bbb9635da9b99a1
Acoliver102 <23olivera@cistercianmail.net>
2022-03-22 15:54:04 -0500

    Rev command (if revert)

---
+++ src/main/java/frc/robot/commands/shooter/RevShooterManage.java
@@ -0,0 +1,27 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Chassis;
+import frc.robot.subsystems.Shooter;
+
+public class RevShooterManage extends CommandBase {
+
+    Shooter mShooter;
+    Chassis mChassis;
+
+    public RevShooterManage(Shooter shooter, Chassis chassis) {
+        mShooter = shooter;
+        mChassis = chassis;
+        addRequirements(mShooter, mChassis);
+    }
+
+    @Override
+    public void execute() {
+        if (mChassis.aiming) {
+            mShooter.setShooter(0.4);
+        } else {
+            mShooter.setShooter(0.0);
+        }
+    }
+    
+}
